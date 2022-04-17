292e24b5f3ee9bb05b371a281d8035ccac03291b
0xMDIV <0x.mdiv@gmail.com>
2022-04-17 20:08:50 +0200

    Added Gender Choices

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+GENDER_CHOICES = {
+    ('M', 'Herr'),
+    ('F', 'Frau'),
+}
+# # Create your models here.
+# class Customer(models.Model):
+#
+#     salutation = models.CharField(max_length=1, choices=GENDER_CHOICES)
+#     last_name = models.CharField(max_length=255, blank=False, null=False, verbose_name='Nachname')
+#     first_name = models.CharField(max_length=255, blank=False, null=False, verbose_name='Vorname')
+#     mail = models.CharField
