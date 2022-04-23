6ff9859d122f75ca9e91fb0b3a5f107212e1cb4a
0xMDIV <0x.mdiv@gmail.com>
2022-04-23 22:30:24 +0200

    Add Admin Page for the Address

--- address/admin.py
+++ address/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Address
+
 
 # Register your models here.
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'street', 'town', 'country')
+
+
