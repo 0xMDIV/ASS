a7fe7b5a7f29c375918d0ae9adf869fc3b12e299
0xMDIV <0x.mdiv@gmail.com>
2022-04-23 21:55:17 +0200

    Add Custom UserModel Admin

--- customer/admin.py
+++ customer/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+from .forms import CustomUserChangeForm, CustomUserCreationForm, SignUpForm
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (('Personal info'), {'fields': ('first_name', 'last_name', 'birthday')}),
+        (('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email',)}
+         ),
+    )
+
+    form = CustomUserChangeForm
+    add_form = SignUpForm
+
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
