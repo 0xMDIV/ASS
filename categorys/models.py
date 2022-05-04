4138f2b56ec918903f9f0a04a8453734fd01c65b
0xMDIV <0x.mdiv@gmail.com>
2022-05-04 22:32:32 +0200

    Add Category Model

--- categorys/models.py
+++ categorys/models.py
@@ -1,3 +1,14 @@
+from datetime import date
+
 from django.db import models
 
+
 # Create your models here.
+from product.models import Product
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, blank=False, unique=True)
+    description = models.TextField(blank=True)
+    created_at = models.DateField(default=date.today())
+    product = models.ForeignKey(Product)
